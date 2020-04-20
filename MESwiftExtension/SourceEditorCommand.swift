532ea565a9bde2a3d374893ef383a24009381ae8
snowimba <a8899308@163.com>
2020-04-20 14:51:30 +0800

    添加删除选择行多余空格的插件选项

--- MESwiftExtension/SourceEditorCommand.swift
+++ MESwiftExtension/SourceEditorCommand.swift
@@ -13,17 +13,38 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
-        let lines = invocation.buffer.lines
-        guard lines.count > 0 else {
+        guard let bundleIdentifier = Bundle.main.bundleIdentifier else {
             completionHandler(nil)
             return
         }
-        for i in 0..<lines.count {
-            guard let line = lines[i] as? String
-                else { continue }
-            if line.trim().isEmpty {
-                invocation.buffer.lines.replaceObject(at: i, with: "\n")
+        switch invocation.commandIdentifier {
+        case bundleIdentifier + ".RemoveEmptyLines":
+            let lines = invocation.buffer.lines as! [String]
+            guard lines.count > 0 else {
+                completionHandler(nil)
+                return
             }
+            for i in 0..<lines.count {
+                let line = lines[i]
+                if line.trim().isEmpty {
+                    invocation.buffer.lines.replaceObject(at: i, with: "\n")
+                }
+            }
+        case bundleIdentifier + ".RemoveEmptySelections":
+            guard let textRange = invocation.buffer.selections.firstObject as? XCSourceTextRange,
+                invocation.buffer.lines.count > 0 else {
+                completionHandler(nil)
+                break
+            }
+            for lineIndex in textRange.start.line...textRange.end.line {
+                guard lineIndex < invocation.buffer.lines.count - 1,
+                    let line = invocation.buffer.lines[lineIndex] as? String
+                    else { break }
+                if line.trim().isEmpty {
+                   invocation.buffer.lines.replaceObject(at: lineIndex, with: "\n")
+                }
+            }
+        default: break
         }
         completionHandler(nil)
     }
